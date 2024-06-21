78df86ffac89af435d7f97885571673165a94ccd
karim-a-khaled <karim.a.s.khaled@gmail.com>
2024-06-22 00:55:36 +0300

    added the exception handler middleware

---
+++ PartTwo.WebAPI/Controllers/BuggyController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartTwo.Data;
+using PartTwo.Entities.Entities;
+using PartTwo.Services.Interfaces;
+using PartTwo.WebAPI.Errors;
+
+namespace PartTwo.WebAPI.Controllers;
+
+public class BuggyController : BaseController
+{
+    private readonly StoreContext _context;
+
+    public BuggyController(StoreContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet("not-found")]
+    public ActionResult GetNotFound()
+    {
+        Product product = _context.Products.Find(4324);
+        
+        if(product is null)
+            return NotFound(new ApiResponse(404));
+        
+        return Ok(product);
+    }
+
+    [HttpGet("servererror")]
+    public ActionResult GetServerError()
+    {
+        Product product = _context.Products.Find(4324);
+
+        var thing = product.ToString();
+
+        return Ok();
+    }
+
+    [HttpGet("badrequest")]
+    public ActionResult GetBadRequest()
+    {
+        return BadRequest(new ApiResponse(400));
+    }
+
+    [HttpGet("badrequest/{id}")]
+    public ActionResult GetNotFoundRequest(int id)
+    { 
+        return Ok();
+    }
+}
